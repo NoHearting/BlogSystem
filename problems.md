d721ca519beccf0ffce283ab4c0683a63ace91fd
NoHearting <1762861794@qq.com>
2020-03-10 20:41:47 +0800

    给后台管理系统添加一个访问目录admin，和blog系统区分开来

--- problems.md
+++ problems.md
@@ -6,4 +6,9 @@
 * **解决：**
     * 编写一个页面的controller，将页面的`src`设置为`<iframe frameborder="0"  th:src="@{/welcome}" class="x-iframe"></iframe>`
     
-    
+##### IDEA中commit代码之后在github上并没有看到提交的代码
+* IDEA提交之后并没有push到github
+* 解决办法：
+    * 右击项目名，点击Git
+    * 进入Repository,点击进入push
+    * 点击右下角的Push
--- src/main/java/com/example/backgroundsystem/controller/BaseController.java
+++ src/main/java/com/example/backgroundsystem/controller/BaseController.java
@@ -3,8 +3,10 @@ package com.example.backgroundsystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class BaseController {
     /**
      * 首页
