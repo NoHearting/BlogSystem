ace2e9df29e031dbbba172818eda121023ccb80b
NoHearting <1762861794@qq.com>
2020-05-18 15:38:20 +0800

    更换了后台系统html模板，并且将模板设置完成

---
+++ src/main/java/com/example/backgroundsystem/controller/BkSysController.java
@@ -0,0 +1,7 @@
+package com.example.backgroundsystem.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BkSysController {
+}
--- src/main/java/com/example/backgroundsystem/controller/CommentController.java
+++ src/main/java/com/example/backgroundsystem/controller/CommentController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 处理评论
+ */
 @Controller
 public class CommentController {
 
